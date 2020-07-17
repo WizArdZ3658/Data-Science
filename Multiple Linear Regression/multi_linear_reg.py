096becee64e32e6a382dfde687aac48bfad4f507
Somnath <p.somnath2599@gmail.com>
2020-07-17 13:35:43 +0530

    Multiple Reg model added

---
+++ Multiple Linear Regression/multi_linear_reg.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# encoding categorical data
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+regressor = LinearRegression()  # with this class don't worry about dummy var trap
+
+"""Backward Elimination is irrelevant in Python, because the Scikit-Learn library automatically takes care of 
+selecting the statistically significant features when training the model to make accurate predictions. """
+
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
